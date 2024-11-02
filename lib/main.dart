0f5e92b22d6dd1f38dd035b0273bac083196b151
Ahmet Kaan <151012025+ahm-tkaan@users.noreply.github.com>
2024-11-02 21:09:12 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:untitled/MainPage.dart';
+import 'package:untitled/main_page.dart';
 
 void main() {
   runApp(
